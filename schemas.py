f58a821321996a711794576bc6df25411b513d80
Ian Farai Madhara <iannyfarai@gmail.com>
2026-09-25 23:05:48 -0400

    added sql alchemy integrations

--- schemas.py
+++ schemas.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Author(BaseModel):
@@ -10,15 +10,18 @@ class Author(BaseModel):
     avatar: str
 
 
-class PostResponse(BaseModel):
-    id: int
-    slug: str
+class PostBase(BaseModel):
     title: str
     subtitle: str
     content: str
     cover_image: str
+    tags: list[str] = []
+
+
+class PostResponse(PostBase):
+    id: int
+    slug: str
     author: Author
-    tags: list[str]
     claps: int
     comments_count: int
     read_time_minutes: int
@@ -26,9 +29,5 @@ class PostResponse(BaseModel):
     updated_at: datetime
 
 
-class PostCreate(BaseModel):
-    title: str
-    subtitle: str
-    content: str
-    cover_image: str
-    tags: list[str] = []
+class PostCreate(PostBase):
+    pass
